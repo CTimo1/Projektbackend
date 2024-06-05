db4e62d3b68bd756e64e6544454bfeae56724a4e
CTimo1 <ctimo665@gmail.com>
2024-06-05 11:38:05 +0200

    first commit

---
+++ start/routes.ts
@@ -0,0 +1,44 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import { exec } from 'child_process';
+import router from '@adonisjs/core/services/router'
+
+
+router.get('/vypni', async () => {
+  exec('raspi-gpio set 17 dl',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+
+
+  return {
+    status: 'OK',
+  }
+})
+
+router.get('/zapni', async () => {
+  exec('raspi-gpio set 17 dh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+
+
+  return {
+    status: 'OK',
+  }
+})
