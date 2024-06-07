f284fe8de39c7f50297d88fa530bae1b8d580856
CTimo1 <ctimo665@gmail.com>
2024-06-07 16:53:19 +0200

    zmena

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 export NODE_ENV=production
 export APP_KEY=nlPFJZlddzfdZ1Q_lgpXLojLcXvhL8PJ
-export HOST=localhost
+export HOST=0.0.0.0
 export PORT=3333
 export LOG_LEVEL=debug
 pinctrl set 17 op
